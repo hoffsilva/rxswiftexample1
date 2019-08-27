431caa03852b8df73fa902124202fbd860c987b9
Hoff Silva <hoff.silva@ebanx.com>
2019-08-27 11:29:36 -0300

    Finish example 1

---
+++ RxSwiftExample1/RxSwiftExample1/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  RxSwiftExample1
+//
+//  Created by Hoff Henry Pereira da Silva on 27/08/19.
+//  Copyright © 2019 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    let disposeBag = DisposeBag()
+    
+    let typpedStringPublishSubject = PublishSubject<String>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.delegate = self
+        self.textField.addTarget(self, action: #selector(MainViewController.startObservable(_:)), for: .editingChanged)
+    }
+
+    @objc private func startObservable(_ textField: UITextField ) {
+        typpedStringPublishSubject.onNext(textField.text ?? "")
+    }
+
+}
+
+extension MainViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        typpedStringPublishSubject.subscribe(onNext: { [weak self] typpedValue in
+            self?.label.text = typpedValue
+        }).disposed(by: disposeBag)
+    }
+    
+}
+
